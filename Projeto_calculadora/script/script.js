8fc150a50028eaa6058869782d89c10f5c8b1214
RaíssaAreal <38378004+Teddy1LeBunny@users.noreply.github.com>
2021-09-22 17:08:36 -0300

    Add files via upload

--- Projeto_calculadora/script/script.js
+++ Projeto_calculadora/script/script.js
@@ -1,17 +1,26 @@
 console.log("Javascript carregado");
-function setUp(){
-        
-    function totalParcial (){
-        let a = document.getElementById("not1").value;  
-        let b = document.getElementById("not2").value;
-        let c = document.getElementById("not3").value;
-        let d = document.getElementById("not4").value;
-        var media = document.getElementById("peso").value; 
 
+function setUp(){
+
+    document.getElementById("resuldadoNotas").value = 0  
+
+    function totalParcial (){
+        let a = parseFloat( document.getElementById("not1").value);  
+        let b = parseFloat( document.getElementById("not2").value);
+        let c = parseFloat( document.getElementById("not3").value);
+        let d = parseFloat( document.getElementById("not4").value);
+        var media =parseFloat(  document.getElementById("peso").value); 
+
+        console.log(a, b, c, d, media)
+    
+        
         var calculo = a + b + c + d;
         var total = calculo / media; 
-        return total 
+
+        return total;
+
     }
+
     document.getElementById("resuldadoNotas").value = totalParcial();
 }
 
